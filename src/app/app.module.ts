3ae1853615e450f42853632901342034067ed901
radek <radekpietrzak102@gmail.com>
2021-08-12 11:07:22 +0200

    add expense-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import { ExpenseListComponent } from './expense/expense-list/expense-list.component';
 
+const routes: Routes = [
+  {
+    component: ExpenseListComponent,
+    path: 'expense-list'
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpenseListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
